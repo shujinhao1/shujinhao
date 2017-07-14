2a3bd7d49795ad4667239f1601a8ebf1e31fde33
shu <shu@shu-PC>
2017-07-14 16:16:56 +0800

    StringUtil

--- src/main/java/com/shu/core/utils/StringUtil.java
+++ src/main/java/com/shu/core/utils/StringUtil.java
@@ -2,9 +2,6 @@ package com.shu.core.utils;
 
 public class StringUtil {
 	public static void main(String[] args) {
-		User user=new User();
-		System.out.println(user==null?true:false);
-		System.out.println(user.equals(null)?true:false);
 	}
 }
 
